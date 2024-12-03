26b7e37b2101c2708412a1111b0f87205598d580
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-12-03 19:10:22 -0300

    feat: added products page

---
+++ app/dashboard/products/page.tsx
@@ -0,0 +1,90 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { MoreHorizontal, PlusCircle, UserIcon } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+export default function ProductsPage() {
+  return (
+    <>
+      <div className="flex items-center justify-end">
+        <Button asChild className="flex items-center gap-x-2">
+          <Link href="/dashboard/product/create">
+            <PlusCircle className="w-3.5 h-3.5" />
+            <span>Add Product</span>
+          </Link>
+        </Button>
+      </div>
+      <Card className="mt-5">
+        <CardHeader>
+          <CardTitle>Products</CardTitle>
+          <CardDescription>
+            Manage your products and view their sales performance
+          </CardDescription>
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Image</TableHead>
+                  <TableHead>Name</TableHead>
+                  <TableHead>Status</TableHead>
+                  <TableHead>Price</TableHead>
+                  <TableHead>Date</TableHead>
+                  <TableHead className="text-right">Actions</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    <UserIcon className="h-16 w-16" />
+                  </TableCell>
+                  <TableCell>Nike air</TableCell>
+                  <TableCell>Active</TableCell>
+                  <TableCell>$299.00</TableCell>
+                  <TableCell>15/06/2024</TableCell>
+                  <TableCell className="text-end">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button size="icon" variant="ghost">
+                          <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </CardContent>
+        </CardHeader>
+      </Card>
+    </>
+  );
+}
