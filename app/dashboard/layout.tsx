1536d1303d564704b402dc9076a0feb8aa69a9f8
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-12-03 18:49:34 -0300

    feat: handle protection to dashboard and added logout btn

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -16,8 +16,24 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+  getKindeServerSession,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function DashboardLayout({ children }: { children: ReactNode }) {
+export default async function DashboardLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || user.email !== "sully.dev.test@gmail.com") {
+    return redirect("/");
+  }
+
   return (
     <div className="flex w-ful flex-col max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <header className="sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-white">
@@ -50,11 +66,14 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
             <DropdownMenuContent align="end">
               <DropdownMenuLabel>My Account</DropdownMenuLabel>
               <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <LogoutLink>Logout</LogoutLink>
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </Sheet>
       </header>
+      <main className="my-5">{children}</main>
     </div>
   );
 }
