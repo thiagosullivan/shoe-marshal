3f008d387a2685baf9ff2481fcfa2f4c0d9c708c
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-12-03 18:54:00 -0300

    feat: added orders page

---
+++ app/dashboard/orders/page.tsx
@@ -0,0 +1,54 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import React from "react";
+
+export default function OrdersPage() {
+  return (
+    <Card>
+      <CardHeader className="px-7">
+        <CardTitle>Orders</CardTitle>
+        <CardDescription>Recent orders from your store!</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Customer</TableHead>
+              <TableHead>Type</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <p className="font-medium">Jan Marshal</p>
+                <p className="hidden md:flex text-sm text-muted-foreground">
+                  teste@teste.com
+                </p>
+              </TableCell>
+              <TableCell>Sale</TableCell>
+              <TableCell>Successfull</TableCell>
+              <TableCell>2024-06-15</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
+}
