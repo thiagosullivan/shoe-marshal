a3881aa8961b385b1d3200dbf154c83f6562117a
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-12-07 21:18:17 -0300

    feat: create and delete banners

---
+++ app/dashboard/banner/[id]/delete/page.tsx
@@ -0,0 +1,40 @@
+import { deleteBanner } from "@/app/actions";
+import { SubmitButtons } from "@/app/components/SubmitButtons";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+export default async function DeleteBannerRoute({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  return (
+    <div className="h-[80vh] w-full flex items-center justify-center">
+      <Card className="max-w-xl">
+        <CardHeader>
+          <CardTitle>Are your absolutely sure?</CardTitle>
+          <CardDescription>
+            This action cannot be undone. This will permanently delete this
+            banner and remove all data from our servers.
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className="w-full flex justify-between">
+          <Button variant="secondary" asChild>
+            <Link href="/dashboard/banner">Cancel</Link>
+          </Button>
+          <form action={deleteBanner}>
+            <input type="hidden" name="bannerId" value={(await params).id} />
+            <SubmitButtons variant="destructive" text="Delete Banner" />
+          </form>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
